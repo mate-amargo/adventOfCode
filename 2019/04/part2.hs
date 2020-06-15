df5b74435374f5108b065ed9fb0299266f028eb7
Juan <00jarg@gmail.com>
2020-06-15 12:23:48 -0300

    Add solution to day 04 part 2

---
+++ 2019/04/part2.hs
@@ -0,0 +1,21 @@
+import Data.List
+
+-- input : 146810-612564
+range = [146810..612564]
+
+digits :: Int -> [Int]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `rem` 10]
+
+nonDecreasingDigits :: Int -> Bool
+nonDecreasingDigits x = foldl (&&) True $ zipWith (>=) (digits x) (0 : digits x)
+
+-- If we first filter the decreasing numbers out, then there's no need to check for adjacent double digits
+-- since all repeating digits must be adjacent.
+hasDoubleDigits :: Int -> Bool
+hasDoubleDigits x = 2 `elem` (map (length) $ group $ digits x)
+
+passwords = filter (hasDoubleDigits) $ filter (nonDecreasingDigits) range
+
+main = do
+  putStrLn $ "There are " ++ (show $ length passwords) ++ " different passwords"
